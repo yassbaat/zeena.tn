ef4dc66366fd6d4b289d8a2ca70c51a651f806f3
yassbaat <baatourx@gmail.com>
2025-02-02 12:18:55 +0100

    added favicon and descriiption

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,26 @@
-import "./globals.css"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import type React from "react" // Import React
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import type React from "react"; // Import React
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Minimalist Landing Page",
-  description: "A super minimalist landing page",
-}
+  title: "Zeena Tunisie",
+  description: "Achetez en toute confiance en ligne en Tunisie",
+  icons: {
+    icon: "/favicon.ico", // Favicon for the site
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="fr">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
-
